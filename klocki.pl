7f2ea4c7068fb3a7ee6ffe09a2b1abf4eda162af
wojtek-gor <73948683+wojtek-gor@users.noreply.github.com>
2022-03-03 13:30:00 +0100

    Add files via upload

---
+++ klocki.pl
@@ -0,0 +1,11 @@
+% program klocki_1
+% na(X,Y)
+% opis: spe³niony, gdy klocek X le¿y na klockuY
+na(c,a).
+na(c,b).
+na(d,c).
+/*
+Informacje o budowie:
+program sk³ada siê z 3 klauzul
+Program zawiera 1 definicjê relacji
+*/
